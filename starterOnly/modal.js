15370093144bf31a1d0c86947544ad2b9ebda039
Laura Nicolay <devnicolay@gmail.com>
2021-07-23 11:04:59 +0200

    Fermeture de la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,7 +11,11 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const btnSubmit = document.getElementById("submit-btn");
+const cross = document.querySelector(".close");
 
+
+// Part modal
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -20,4 +24,19 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal event
+btnSubmit.addEventListener("click", sendFormBtn);
 
+// close modal form
+function sendFormBtn() {
+  modalbg.style.display = "none";
+  alert("Merci! Votre réservation a été reçue.");
+}
+
+// close modal event with the cross
+cross.addEventListener("click", closeForm);
+
+// close modal form with the cross
+function closeForm() {
+  modalbg.style.display = "none";
+} 
