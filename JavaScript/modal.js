781f7839cf33ab1741aae3b916f8850235c8f793
Laura Nicolay <devnicolay@gmail.com>
2021-08-04 15:20:45 +0200

    Ajout de commentaires dans le JS

--- JavaScript/modal.js
+++ JavaScript/modal.js
@@ -1,11 +1,17 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  var x = document.getElementById("myTopnav"); 
+  // x will be element with id "myTopnav"
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
   }
+  // if x has class with the name "topnav" concatenate of class "reponsive" who is in the class "topnav"
+  // and apply the style of CSS for max-width: 768px
+  // else x will be class with the name "topnav" and apply th estyle of CSS for max-width: 540px
 }
+
+
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
@@ -18,26 +24,28 @@ const btnClose = document.getElementById("closebtn");
 
 
 // part modal
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
 // launch modal form
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// Every time we click on modalBtn, launch function launchModal
 function launchModal() {
-  modalbg.style.display = "block";
-}
+  modalbg.style.display = "block"; 
+} // function launchModal makes appear modalbg
+
 
 // close modal form with the cross
 crossClose.addEventListener("click", closeForm);
-
+// When click on crossClose, launch function closeForm
 function closeForm() {
   modalbg.style.display = "none";
-}
+} // function closeForm makes disappear modalbg
+
 
 // launch success message when submit form
 function displaySuccessMessage() {
   windowFormSend.style.display = "flex";
   modalBody.style.display= "none";
-}
+} // function displaySuccessMessage makes appear windowFormSend and disappear modalBody
+
 
 // When click on button id= closebtn, launch function closeForm 
 btnClose.addEventListener("click", closeForm);
--- JavaScript/validationForm.js
+++ JavaScript/validationForm.js
@@ -3,7 +3,7 @@ const firstName = document.getElementById("first");
 const lastName = document.getElementById("last");
 const email = document.getElementById("email");
 const birthdate = document.getElementById("birthdate");
-const quantityTournament = document.getElementById("quantity")
+const quantityTournament = document.getElementById("quantity");
 const locationBtnRadios = document.querySelectorAll("[name=location]");
 const termsOfUse = document.getElementById("checkbox1");
 const form = document.getElementById("form");
@@ -12,28 +12,30 @@ const form = document.getElementById("form");
 
 // Form part
 // Input firstname
-firstName.addEventListener("blur", firstNameCheck);
-
+firstName.addEventListener("blur", firstNameCheck); 
+// When quit field firstName, launch function firstNameCheck
 function firstNameCheck() {
   const alertMsg = document.querySelector(".firstname .alert-msg");
-  if (firstName.value.trim().length >= 2) {
-    alertMsg.style.display = "none";
-    firstName.classList.remove("border-red");
+  if (firstName.value.trim().length >= 2) { // the value of firstName must have 2 caracters or more
+    alertMsg.style.display = "none"; // alertMsg does not appear
+    firstName.classList.remove("border-red"); // remove the class "border-red"
     return true;
   }
   else {
-    alertMsg.style.display = "flex";
-    firstName.classList.add("border-red");
+    alertMsg.style.display = "flex"; // alertMsg appear
+    firstName.classList.add("border-red"); // add the class "border-red"
     return false;
   }
 }
  
-// Input lastname
-lastName.addEventListener("blur", lastNameCheck);
 
+
+// Input lastname
+lastName.addEventListener("blur", lastNameCheck); 
+// When quit field lastName, launch function lastNameCheck
 function lastNameCheck() {
   const alertMsg = document.querySelector(".lastname .alert-msg");
-  if (lastName.value.trim().length >= 2) {
+  if (lastName.value.trim().length >= 2) { // the value of lastName must have 2 caracters or more
     alertMsg.style.display = "none";
     lastName.classList.remove("border-red");
     return true;
@@ -45,12 +47,15 @@ function lastNameCheck() {
   }
 }
 
-// Input email
-email.addEventListener("blur", emailCheck);
 
+
+// Input email
+email.addEventListener("blur", emailCheck); 
+// When quit field email, launch function emailCheck
 function emailCheck() {
   const alertMsg = document.querySelector(".email .alert-msg");
   const mailFormat = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$/;
+  // mailFormat is the format that the mail field must have, like letters, numbers, symbols @ letters, numbers .(dot) letters, numbers
   if (email.value.match(mailFormat)) {
     alertMsg.style.display = "none";
     email.classList.remove("border-red");
@@ -63,12 +68,15 @@ function emailCheck() {
   }
 }
 
-// Input birthdate
-birthdate.addEventListener("blur", birthdateCheck);
 
+
+// Input birthdate
+birthdate.addEventListener("blur", birthdateCheck); 
+// When quit field birthdate, launch function birthdateCheck
 function birthdateCheck() {
   const alertMsg = document.querySelector(".birthdate .alert-msg");
   const birthdateFormat = /[0-9]{4}-[0-9]{2}-[0-9]{2}/;
+  // birthdateFormat is the format that the birthdate field must have, a number format like "0000 - 00 - 00"
   if (birthdate.value.match(birthdateFormat)) {
     alertMsg.style.display = "none";
     birthdate.classList.remove("border-red");
@@ -81,12 +89,15 @@ function birthdateCheck() {
   }
 }
 
-// Input number of tournaments
-quantityTournament.addEventListener("change", quantityTournamentCheck);
 
+
+// Input number of tournaments
+quantityTournament.addEventListener("change", quantityTournamentCheck); 
+// When modify field quantityTournament, launch function quantityTournamentCheck
 function quantityTournamentCheck() {
   const alertMsg = document.querySelector(".quantityTournament .alert-msg");
   const quantityFormat = /[0-9]/;
+  // quantityFormat is the format that the quantityTournament field must have, a format of number
   if (quantityTournament.value.match(quantityFormat) && quantityTournament.value >= 0) {
     alertMsg.style.display = "none";
     quantityTournament.classList.remove("border-red");
@@ -99,14 +110,19 @@ function quantityTournamentCheck() {
   }
 }
 
+
+
 //Button radio for location
 locationBtnRadios.forEach((locationBtnRadio) => {
   locationBtnRadio.addEventListener("change", locationBtnRadioCheck);
 })
+// When modify one of all elements of locationBtnRadios (one of all of six button radio), launch function locationBtnRadioCheck
 function locationBtnRadioCheck() {
   const alertMsg = document.querySelector(".location .alert-msg");
   const checkboxIconLocation = document.querySelectorAll(".checkbox-label .checkbox-icon");
+  // checkboxIconLocation will be the elements in class "checkbox-icon" that are in "checkbox-label"
   const isRadioBtnChecked = Array.from(locationBtnRadios).some((radioBtn) => radioBtn.checked)
+  // One of element of array locationBtnRadios (one of six button radio) is checked
   if (isRadioBtnChecked) {
     alertMsg.style.display = "none";
     Array.from(checkboxIconLocation).forEach((checkbox) => {
@@ -123,12 +139,15 @@ function locationBtnRadioCheck() {
   }
 }
 
-// Checkbox terms of use
-termsOfUse.addEventListener("change", termsOfUseCheck);
 
+
+// Checkbox terms of use
+termsOfUse.addEventListener("change", termsOfUseCheck); 
+// When modify field termsOfUse, launch function termsOfUseCheck
 function termsOfUseCheck(){
   const alertMsg = document.querySelector(".terms-of-use .alert-msg");
   const checkboxIconTerms = document.getElementById("checkbox-icon-terms");
+  // checkboxIconTerms will be element with id "checkbox-icon-terms"
   if (termsOfUse.checked) {
     alertMsg.style.display = "none";
     checkboxIconTerms.style.border = "none";
@@ -145,11 +164,11 @@ function termsOfUseCheck(){
 
 // Form validation
 form.addEventListener("submit", formValidation);
-
+// When submit form, launch formValidation
 function formValidation(event) {
-  event.preventDefault();
-  const isFirstNameValid = firstNameCheck();
-  const isLastNameValid = lastNameCheck();
+  event.preventDefault(); // action for default of "submit" don't be effected
+  const isFirstNameValid = firstNameCheck(); 
+  const isLastNameValid = lastNameCheck(); // variable isLastNameValid will be function lastNameCheck
   const isEmailValid = emailCheck();
   const isBirthdateValid = birthdateCheck();
   const isQuantityTournamentValid = quantityTournamentCheck();
@@ -159,4 +178,5 @@ function formValidation(event) {
       form.reset();
       displaySuccessMessage();
       }
+      // if all fields are valid, reset form and launch function displaySuccessMessage
 }
